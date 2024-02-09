c3a86cf67a4ece9c2e756e7066eb0073c4d1fb15
suhe.spt@gmail.com <suhe.spt@gmail.com>
2024-02-09 11:01:04 +0700

    menambhakan user seeder dan modi tampilan di dashboard

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,6 +42,9 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        $user->assignRole('peternak');
+        // dd($user);
+
         event(new Registered($user));
 
         Auth::login($user);
